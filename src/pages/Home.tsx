b23d9039b818b4b5e06123b6bdb6abea54719e66
brenermr <brenercaique0806@gmail.com>
2021-11-21 19:54:07 -0300

    mySkills

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {
     Text, 
     View, 
@@ -12,16 +12,44 @@ import {
 import { Button } from '../components/Button'
 import { CardSkill } from '../components/CardSkill'
 
+ {/* My Interfaces */}
+
+ interface SkillData{
+     id:string;
+     name:string;
+ }
+
 export function Home(){
-    const [newSkill, setNewSkill] = useState()
-    const [mySkills, setMySkills] = useState([])
+    {/* States */}
+    const [newSkill, setNewSkill] = useState('')
+    const [mySkills, setMySkills] = useState<SkillData[]>([])
+    const [grettings, setGrettings] = useState('')
 
     function handleAddNewSkill(){
-       setMySkills(oldState =>[...oldState, newSkill])
+        const data={
+            id:String(new Date().getTime()),
+            name: newSkill,
+        }
+       setMySkills(oldState =>[...oldState, data])
     }
+    useEffect(()=>{
+        const currentHours = new Date().getHours()
+        let morning = currentHours < 12
+        let afternoon = currentHours >=12 && currentHours < 18
+        let night = currentHours >= 19 && currentHours < 23
+        console.log(currentHours)
+        if(morning){
+            setGrettings('Bom dia')
+        }else if(afternoon){
+            setGrettings('Boa Tarde')
+        }else if(night){
+            setGrettings('Boa Noite')
+        }
+    },[])
     return(
         <View style={styles.container}>
             <Text style={styles.title}>Welcome, Caique!</Text>
+            <Text>{grettings}</Text>
             <TextInput 
             placeholder='Your Skill' 
             placeholdeTextColor='#555' 
@@ -56,9 +84,9 @@ export function Home(){
             <FlatList
                 showsVerticalScrollIndicator={false}
                 data={mySkills}
-                keyExtractor={item=>item}
+                keyExtractor={item=>item.id}
                 renderItem={({item})=>(
-                    <CardSkill propSkills={item}/>
+                    <CardSkill propSkills={item.name}/>
                 )}
             />
         </View>
